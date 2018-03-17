09450b4f43be6abf28ab1eac3713902f8affd74c
xiaoyouPrince <xiaoyouPrince@163.com>
2018-03-17 23:57:16 +0800

    添加超级猜图的一些资源和解析问题数据

--- 超级猜图/XYSuperGuessPic/XYSuperGuessPic/ViewController.swift
+++ 超级猜图/XYSuperGuessPic/XYSuperGuessPic/ViewController.swift
@@ -10,13 +10,34 @@
 //  Copyright © 2018年 xiaoyouPrince. All rights reserved.
 //
 
+
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    /// 属性懒加载
+    fileprivate lazy var questions : [XYQuestion] = { [unowned self] in
+        
+        var questions = [XYQuestion]()
+        
+        let path : String = Bundle.main.path(forResource: "questions", ofType: "plist")!
+        let data = NSArray(contentsOfFile: path)!
+        for dict in data {
+            let question = XYQuestion(dict: dict as! [String : AnyObject])
+            questions.append(question)
+        }
+        return questions
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /// 1. load data
+        let aaa = self.questions
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,4 +47,23 @@ class ViewController: UIViewController {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- 超级猜图/XYSuperGuessPic/XYSuperGuessPic/XYQuestion.swift
+++ 超级猜图/XYSuperGuessPic/XYSuperGuessPic/XYQuestion.swift
@@ -14,5 +14,28 @@ import UIKit
 class XYQuestion: NSObject {
     
     
+    var answer : String?    /// 答案
+    var icon   : String?    /// 对应图片
+    var title  : String?    /// 对应标题
+    var options: Array<String>?     /// 带选项
+    
+    
+    /// 重写父类初始化方法
+    override init() {
+        super.init()
+    }
+    
+    /// 自定义初始化方法
+    init( dict : [String : AnyObject]) {
+        super.init()
+        
+        /// KVC 赋值
+        setValuesForKeys(dict)
+    }
+    
+    /// KVC for undefined key
+    override func setValue(_ value: Any?, forUndefinedKey key: String) {
+        
+    }
 
 }
